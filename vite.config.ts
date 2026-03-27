5dac5b8d8f9b73ad7612f4e34f86910ef838987c
codejie <codejie@gmail.com>
2026-03-27 15:19:46 +0800

    dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '3000'),
+    cors: true,
+  },
   plugins: [
     vue(),
     tailwindcss(),
